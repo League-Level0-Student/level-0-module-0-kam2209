b8c1707385a9677c39a8caeb83fdb16e035dd003
root <root@84fb004fbb63>
2021-02-06 02:29:59 +0000

    World domination, Quiz Game In progess

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -1,18 +1,28 @@
 package _03_print_and_popups._3_world_domination;
 
-
 import javax.swing.JOptionPane;
 
 public class WorldDomination {
-	
+
 	public static void main(String[] args) {
-		
+
 		// 1. Ask the user if they know how to write code.
+		String answer = JOptionPane.showInputDialog("Do you know how to write code");
 
 		// 2. If they say "yes", tell them they will rule the world.
+		if (answer.equalsIgnoreCase("yes")) {
+			JOptionPane.showMessageDialog(null, "you will rule the world");
+		}
 
 		// 3. Otherwise, tell them to sign up for classes at The League.
+		else {
+			JOptionPane.showMessageDialog(null, "sign up for classes at The League");
+		}
 
 	}
-}
 
+	private static String JOptionPane() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
